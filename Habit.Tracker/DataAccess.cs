16257f9278986770f5ccc3fb2a99751dfcaeaf90
Lewis Hardcastle <personal@lewishardcastle.io>
2026-01-20 19:43:43 +0000

    Create DB on startup

---
+++ Habit.Tracker/DataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using Microsoft.Data.Sqlite;
+using System.Linq.Expressions;
+
+namespace Habit.Tracker
+{
+    internal class DataAccess
+    {
+        string _connectionString;
+
+        public DataAccess(string dbName)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings[dbName].ConnectionString;
+        }
+        
+        public void TestConnection()
+        {
+            try
+            { 
+                var con = new SqliteConnection(_connectionString);
+                con.Open();
+                con.Close();
+
+                Console.WriteLine("Connection successful.");
+            }
+            catch (Exception ex)
+            {                
+                Console.WriteLine("Connection failed: " + ex.Message); 
+            }
+        }
+
+    }
+}
--- Habit.Tracker/obj/Debug/net10.0/Habit.Tracker.AssemblyInfo.cs
+++ Habit.Tracker/obj/Debug/net10.0/Habit.Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Habit.Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3203e3579d593abb32bafaabef96490144110d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Habit.Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Habit.Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
