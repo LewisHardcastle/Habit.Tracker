ac5a54cd46671fb529f66034d74d92893b1ff34c
Lewis Hardcastle <Lewis.Hardcastle@howdengrp.com>
2026-08-06 14:17:54 +0100

    Add unit tests

---
+++ Habit.Tracker.Test/Habit.Tracker.Test.cs
@@ -0,0 +1,78 @@
+﻿using Habit.Tracker;
+using System.Configuration;
+
+namespace Habit.Tracker.Test
+{
+    public class HabitTrackerTest
+    {
+        [TestCase("abc")]
+        [TestCase("!@#")]
+        [TestCase("12a")]
+        [TestCase("a12")]
+        [TestCase("\n")]
+        [TestCase(" ")]
+        [TestCase("")]
+        public void GivenInput_WhenIsNotANumber_ThenReturnFalse(string userInput)
+        {
+            //Arrange & Act
+            var result = UserInputHandler.CheckStringIsANumber(userInput);
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+
+        [TestCase("1")]
+        [TestCase("0")]
+        [TestCase("99")]
+        [TestCase("100")]
+        public void GivenInput_WhenIsANumber_ThenReturnTrue(string userInput)
+        {
+            //Arrange & Act
+            var result = UserInputHandler.CheckStringIsANumber(userInput);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+        [TestCase("24/06/1997")]
+        [TestCase("01/12/2024")]
+        public void GivenString_WhenIsDate_ThenReturnDateTime(string date)
+        {
+            //Arrange & Act
+            var result = UserInputHandler.CheckStringIsPastOrPresentDate(date);
+
+            //Assert
+            Assert.That(result, Is.InstanceOf<DateTime>());
+        }
+
+        [TestCase("01/13/1997")]
+        [TestCase("01/25/2024")]
+        [TestCase("abc")]
+        [TestCase("")]
+        [TestCase("\n")]
+        public void GivenString_WhenIsNotDate_ThenReturnNull(string date)
+        {
+            //Arrange & Act
+            var result = UserInputHandler.CheckStringIsPastOrPresentDate(date);
+
+            //Assert
+            Assert.That(result, Is.Null);
+        }
+
+        private static IEnumerable<string> FutureDates()
+        {
+            yield return DateTime.Now.AddDays(1).ToString("dd/MM/yyyy");
+            yield return DateTime.Now.AddDays(30).ToString("dd/MM/yyyy");
+            yield return DateTime.Now.AddYears(1).ToString("dd/MM/yyyy");
+        }
+
+        [TestCaseSource(nameof(FutureDates))]
+        public void GivenStringDate_WhenDateIsFuture_ThenReturnNull(string date)
+        {
+            // Arrange & Act
+            var result = UserInputHandler.CheckStringIsPastOrPresentDate(date);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+    }
+}
--- Habit.Tracker/Program.cs
+++ Habit.Tracker/Program.cs
@@ -11,6 +11,8 @@ db.CreateTable();
 
 bool userIsActive = true;
 
+UserInputHandler userInputHandler = new UserInputHandler();
+
 while(userIsActive)
 {
     Console.WriteLine("Welcome to Habit Tracker!");
@@ -22,97 +24,5 @@ while(userIsActive)
 
     string userInput = Console.ReadLine();
 
-    switch (userInput.ToUpper())
-    {
-        case "C":
-            Console.WriteLine("Enter the habit name");
-            string habitName = Console.ReadLine();
-            Console.WriteLine("Enter how many times you have completed the habit");
-            string habitOccurrence = Console.ReadLine();
-
-            while (!habitOccurrence.All(char.IsAsciiDigit))
-            {
-                Console.WriteLine("Please only insert a number");
-                habitOccurrence = Console.ReadLine();
-            }
-
-            int habitOccurrenceNum;
-            Int32.TryParse(habitOccurrence, out habitOccurrenceNum);
-
-            DateTime habitOccurrenceDate;
-
-            while (true)
-            {
-                Console.WriteLine("Enter a date (dd/mm/yyyy): ");
-                string input = Console.ReadLine();
-
-                if (DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out habitOccurrenceDate))
-                {
-                    Console.WriteLine($"Valid date: {habitOccurrenceDate:dd MMMM yyyy}");
-                    break;
-                }
-
-                Console.WriteLine("Invalid date. Please try again.");
-            }
-
-            HabitModel newHabit = new HabitModel
-            {
-                Name = habitName,
-                Occurrence = new Occurrence
-                {
-                    Date = habitOccurrenceDate,
-                    Number = habitOccurrenceNum
-                }
-            };
-
-            db.InsertHabit(newHabit);
-
-            break;
-        case "R":
-            var habits = db.ListAllHabits();
-
-            foreach (HabitModel habit in habits)
-            {
-                Console.WriteLine($"{habit.Name} has been done {habit.Occurrence.Number} times");
-            }
-            break;
-        case "U":
-            // Update occurrence
-            Console.WriteLine("Enter the habit name you want to update");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter the the number of times you have now completed the habit");
-            string newOccurence = Console.ReadLine();
-
-            while (!newOccurence.All(char.IsAsciiDigit))
-            {
-                Console.WriteLine("Please only insert a number");
-                newOccurence = Console.ReadLine();
-            }
-
-            int newOccurenceOut;
-            Int32.TryParse(newOccurence, out newOccurenceOut);
-
-            db.UpdateHabitOccurrence(name, newOccurenceOut);
-
-            break;
-        case "D":
-            // Delete habit
-            Console.WriteLine("Which habit would you like to delete?");
-            string habitToDelete = Console.ReadLine();
-            db.DeleteHabit(habitToDelete);
-            break;
-        default:
-            //
-            break;
-    }
-
-    Console.WriteLine("press Y to continue using the habit tracker or N to exit");
-    string checkUserIsActive = Console.ReadLine().ToUpper();
-    if (checkUserIsActive == "Y")
-    {
-        userIsActive = true;
-    } else
-    {
-        System.Environment.Exit(0);
-    }
+    userInputHandler.handleUserInput(userInput, userIsActive, db);
 }
